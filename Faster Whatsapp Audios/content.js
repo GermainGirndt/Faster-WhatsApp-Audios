386c921f186dde36c400f71e780ee5263cca6685
GermainPereira <pereira.germain@outlook.com>
2020-04-26 11:08:10 -0300

    Added turn off feature and redesigned

--- Faster Whatsapp Audios/content.js
+++ Faster Whatsapp Audios/content.js
@@ -1,24 +1,38 @@
-console.log("Activated SpeedChanger extension.")
-
 
 const interval = setInterval(() => {
     const header = document.querySelector("._3auIg");
 
     if (header) {
-        console.log(header)
         clearInterval(interval)
     
     const button = document.createElement("button");
     button.innerHTML = "2x"
     button.classList.add("doubleSpeedButton")
 
+    var button_state = 0;
     button.addEventListener("click", () => {
-        const audios = document.querySelectorAll("audio");
-        console.log(audios)
-        audios.forEach((audio) => {
-            console.log(audio);
-            audio.playbackRate = 2
-        })
+
+        if (button_state == 0) {
+            button.style.backgroundColor = "lightgray";
+            button.style.color = "#222222";
+            button.style.border = "1px solid #999";
+            button.style.borderRadius = "30%";
+            const audios = document.querySelectorAll("audio");
+            audios.forEach((audio) => {
+                audio.playbackRate = 2
+            })
+            button_state = 1
+        } else {
+            button.style.backgroundColor = null;
+            button.style.color = null;
+            button.style.border = null;
+            button.style.borderRadius = null;
+            const audios = document.querySelectorAll("audio");
+            audios.forEach((audio) => {
+                audio.playbackRate = 1
+            })
+            button_state = 0
+        }
 
     })
 
