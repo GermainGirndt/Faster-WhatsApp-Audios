2bfacda8e753ee05efd19f7c88885f10e34dce22
GermainPereira <pereira.germain@outlook.com>
2020-04-26 16:16:37 -0300

    Added stop loop. Cleaned code.

--- Faster Whatsapp Audios/content.js
+++ Faster Whatsapp Audios/content.js
@@ -1,57 +1,79 @@
-
+const taskbarTag = "._3auIg"
+var buttonState = 0;
 const interval = setInterval(() => {
-    const header = document.querySelector("._3auIg");
+    const header = document.querySelector(taskbarTag);
 
     if (header) {
         clearInterval(interval)
-    
+        
+        const button = createButton()
+
+        button.addEventListener("click", () => {
+            if (buttonState == 0) {
+                buttonState = 1
+                turnOnDoubleSpeed(button)
+            } else {
+                buttonState = 0
+                turnOffDoubleSpeed(button)
+            }
+        })
+        header.appendChild(button)
+    }
+}, 2000)
+
+
+function createButton() {
     const button = document.createElement("button");
     button.innerHTML = "2x"
     button.classList.add("doubleSpeedButton")
+    return button
+}
 
-    var button_state = 0;
-    button.addEventListener("click", () => {
-
-        if (button_state == 0) {
-            turn_on_doubled_playback_rate(button)
-            button_state = 1
-        } else {
-            turn_off_doubled_playback_rate(button)
-            button_state = 0
-        }
-
-    })
-
-    header.appendChild(button)
-
-    }
-
-}, 1000)
+function turnOnDoubleSpeed(button) {
+    applyStyleTurnedOn(button)
+    setDoubleSpeedLoop()
+}
 
 
-
-function turn_on_doubled_playback_rate(button) {
-    console.log("Turned on")
+function applyStyleTurnedOn(button) {
     button.style.backgroundColor = "lightgray";
     button.style.color = "#222222";
     button.style.border = "1px solid #999";
     button.style.borderRadius = "30%";
-    const audios = document.querySelectorAll("audio");
-    audios.forEach((audio) => {
-        audio.playbackRate = 2
-    })
 }
 
+function setDoubleSpeedLoop() {
+    const doubleSpeedLoop = setInterval(function(){
+        let audios = document.querySelectorAll("audio");
+        audios.forEach((audio) => {
+        if (buttonState == 0) {
+            clearInterval(doubleSpeedLoop)    
+        }
+        audio.playbackRate = 2
+        })
+    }, 1000)
+}
 
+function turnOffDoubleSpeed(button) {
+    applyStyleTurnedOff(button)
+    setNormalSpeedLoop()
+}
 
-function turn_off_doubled_playback_rate(button) {
-    console.log("Turned off")
+function applyStyleTurnedOff(button) {
     button.style.backgroundColor = null;
     button.style.color = null;
     button.style.border = null;
     button.style.borderRadius = null;
-    const audios = document.querySelectorAll("audio");
-    audios.forEach((audio) => {
+}
+
+function setNormalSpeedLoop(){
+    const setNormalSpeedLoop = setInterval(function(){
+        let audios = document.querySelectorAll("audio");
+        audios.forEach((audio) => {
+        if (buttonState == 1) {
+            clearInterval(setNormalSpeedLoop)    
+        }
         audio.playbackRate = 1
-    })
+        })
+    }, 1000)
 }
